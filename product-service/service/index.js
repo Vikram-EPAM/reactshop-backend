24be79f0f52cfca129f14c977f3dbc8b29f6cef8
vdokkupalle-ebsco <75068341+vdokkupalle-ebsco@users.noreply.github.com>
2022-07-24 10:43:39 +0530

    Added join for stocks

--- product-service/service/index.js
+++ product-service/service/index.js
@@ -1,14 +1,26 @@
 export const getProducts = async (client) => {
   await client.connect();
-  const result = await client.query("SELECT * FROM products");
+  const result = await client.query(
+    `SELECT 
+     id, title, description, price, count 
+     FROM products 
+     INNER JOIN stocks 
+     ON products.id = stocks.product_id`
+  );
   await client.end();
   return result.rows;
 };
 export const getProductById = async (id, client) => {
   await client.connect();
-  const result = await client.query("SELECT * FROM products WHERE id = $1", [
-    id,
-  ]);
+  const result = await client.query(
+    `SELECT 
+     id, title, description, price, count 
+     FROM products 
+     INNER JOIN stocks 
+     ON products.id = stocks.product_id
+     WHERE id = $1`,
+    [id]
+  );
   await client.end();
   return result.rows[0];
 };
